560a47043098f64c343cda33abcb16b318af0c08
Ilja Tkachuk <ilja@heathmont.net>
2019-05-29 15:51:40 +0300

    init

---
+++ lib/comparable_decimal.ex
@@ -0,0 +1,18 @@
+defmodule ComparableDecimal do
+  @moduledoc """
+  Documentation for ComparableDecimal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ComparableDecimal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/comparable_decimal_test.exs
@@ -0,0 +1,8 @@
+defmodule ComparableDecimalTest do
+  use ExUnit.Case
+  doctest ComparableDecimal
+
+  test "greets the world" do
+    assert ComparableDecimal.hello() == :world
+  end
+end
